46cb65331a51ce5e65f661cb8603690169601ac5
Oscar Tuff <90379013+oTuff@users.noreply.github.com>
2025-04-14 22:47:44 +0200

    Setup data visualization

---
+++ plots/flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+        stdenv = pkgs.stdenv;
+      in
+      rec {
+        devShell =
+          with pkgs;
+          mkShell {
+            nativeBuildInputs = [
+              uv
+            ];
+          };
+      }
+    );
+
+}
