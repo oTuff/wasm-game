a299707f98bf6858f7506cab571e82b4b6fe455d
Oscar Tuff <90379013+oTuff@users.noreply.github.com>
2025-04-18 14:56:58 +0200

    Add asset loading

--- go-ebitengine/main.go
+++ go-ebitengine/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"image/png"
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	Sprite *ebiten.Image
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	op := &ebiten.DrawImageOptions{}
+
+	op.GeoM.Scale(0.5, 0.5)
+
+	screen.DrawImage(g.Sprite, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -22,9 +30,21 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
+	// Load image
+	imageFile, err := os.Open("../assets/bunny.png")
+	if err != nil {
+		fmt.Println("error opening image")
+	}
+	defer imageFile.Close()
+
+	imageData, err := png.Decode(imageFile)
+	if err != nil {
+		fmt.Println("error decoding image data")
+	}
+
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowTitle("BunnyMark Go Ebitengine")
+	if err := ebiten.RunGame(&Game{Sprite: ebiten.NewImageFromImage(imageData)}); err != nil {
 		log.Fatal(err)
 	}
 }
